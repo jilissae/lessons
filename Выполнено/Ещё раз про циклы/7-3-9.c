5d9629c7f43517984f8719b74eefc63a55b97a5f
jilissae <jilissae@21-school.ru>
2024-01-03 19:35:49 +0300

    8.1 is ready

--- Выполнено/Ещё раз про циклы/7-3-9.c
+++ Выполнено/Ещё раз про циклы/7-3-9.c
@@ -1,19 +1,49 @@
+/*
+Напишите программу, которая выводит на экран числа от 1 до 100.
+При этом вместо чисел, кратных трем, программа должна выводить слово «Fizz», а вместо чисел, кратных пяти — слово «Buzz».
+Если число кратно и 3, и 5, то программа должна выводить слово «FizzBuzz»
+*/
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	int a = 0, sum = 0;
+// 	while (a != 0)
+// 	{
+// 		scanf("%d", &a);
+// 	}
+// 	scanf("%d", &a);
+// 	sum += a;
+// 	printf("%d ", a);
+// 	while (a != 0)
+// 	{
+// 		scanf("%d", &a);
+// 		sum += a;
+// 		printf("%d ", a);
+// 	}
+// 	printf("%d ", sum);
+// }
+
 #include <stdio.h>
 int main(void)
 {
-	int a = 0, sum = 0;
-	while (a != 0)
+	for (int i = 1; i <= 100; i++)
 	{
-		scanf("%d", &a);
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i % 3 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
 	}
-	scanf("%d", &a);
-	sum += a;
-	printf("%d ", a);
-	while (a != 0)
-	{
-		scanf("%d", &a);
-		sum += a;
-		printf("%d ", a);
-	}
-	printf("%d ", sum);
 }
