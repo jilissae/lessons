69d491b627a60240405538c4c3e9137523592db7
jilissae <jilissae@21-school.ru>
2024-02-08 15:38:20 +0200

    error

--- C/Символьные строки/9-2-11.c
+++ C/Символьные строки/9-2-11.c
@@ -2,23 +2,28 @@
 #include <string.h>
 #define N 120
 
-int main (void){
-    char a[N],temp[N];
-    int i=0,j=0,k=0;
-    fgets(a,N,stdin);
-    while (a[i]!='\0'){
+int main(void)
+{
+    char a[N], temp[N];
+    int i = 0, j = 0, k = 0;
+    fgets(a, N, stdin);
+    while (a[i] != '\0')
+    {
 
-        while (a[i]==' ') i++;
+        while (a[i] == ' ')
+            i++;
 
-        while (a[i]!=' ' || a[i]!='\0'){
-            temp[j++]=a[i++];
+        while (a[i] != ' ' && a[i] != '\0')
+        {
+            temp[j++] = a[i++];
         }
-        temp[j]='\0';
-        k=strlen(temp);
-        if (k>1 && a[k-1]==a[0]){
+        temp[j] = '\0';
+        k = strlen(temp);
+        if (k > 1 && a[k - 1] == a[0])
+        {
             printf("%s", temp);
-
+            printf(" ");
         }
-        j=0;
+        j = 0;
     }
 }
